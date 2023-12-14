a5f1ad79a584572e1c1b616d6558edb412e85b93
AllanLps <allanlps20@gmail.com>
2023-12-13 21:44:16 -0300

    commit02

---
+++ src/components/comment/Comment.jsx
@@ -0,0 +1,36 @@
+import styles from './Comment.module.css';
+import { Trash, ThumbsUp } from "phosphor-react"
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://i.pinimg.com/564x/63/c7/9e/63c79e7b326a066dbf1c0165fb08ae8a.jpg" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Gohan</strong>
+
+                            <time title='08 de Dezembro 2023' dateTime='2023-08-12 16:45'>Cerca de 1h atrás</time>
+
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={22}/>
+                        </button>
+                    </header>
+
+
+                    <p>Muito bom dev, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp size={22}/>
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from '../comment/Comment'
 import styles from './Post.module.css'
 
 export function Post() {
@@ -30,6 +31,21 @@ export function Post() {
                         <a href="#">#novoprojeto</a>{' '}
                         <a href="#">#nlw</a>
                     </p>
+                </div>
+
+                <form className={styles.commentForm}>
+                    <strong>Deixe seu feedback</strong>
+                    <textarea
+                        placeholder='Deixe um comentário'
+                    />
+                    <footer>
+                        <button type="submit">Publicar</button>
+                    </footer>
+
+                </form>
+                <div className={styles.commentList}>
+                    <Comment/>
+
                 </div>
 
             </article>
